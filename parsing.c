cf636dbcf88e022a718b71f89876b4fccfd8ccac
kishan-98 <kishan.sangani@students.iiit.ac.in>
2017-08-25 19:59:44 +0530

    parsing beginning(2)

--- parsing.c
+++ parsing.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include"parsong.h"
 
-char* parse()
+char* parse(){
+
+}
---
+++ parsing.h
@@ -0,0 +1,4 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"global_variables.h"
